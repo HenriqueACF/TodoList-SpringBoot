71501112980ab1da5eedcb5c390000750fc8fad7
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-10-13 17:26:47 -0300

    feat: iniciando estrutura de tasks

---
+++ src/main/java/com/henriqueacf/todlist/filter/FilterTaskAuth.java
@@ -0,0 +1,17 @@
+package com.henriqueacf.todlist.filter;
+
+
+import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FilterTaskAuth implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+    //executa ação
+    }
+}
---
+++ src/main/java/com/henriqueacf/todlist/task/ITaskRepository.java
@@ -0,0 +1,8 @@
+package com.henriqueacf.todlist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+}
