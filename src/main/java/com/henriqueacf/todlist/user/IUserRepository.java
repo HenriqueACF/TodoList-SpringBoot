edac7d009ff68ec04d2d43dc59aa6b79fe2b6629
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-10-13 11:28:40 -0300

    feat: conectando e acessando o banco de dados

---
+++ src/main/java/com/henriqueacf/todlist/user/IUserRepository.java
@@ -0,0 +1,7 @@
+package com.henriqueacf.todlist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+}
