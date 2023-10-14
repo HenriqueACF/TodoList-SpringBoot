8ce5802028567ed8f524eddcd809fb7e2ae5313c
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-10-14 16:57:15 -0300

    feat: passando idUser na criação de task

--- src/main/java/com/henriqueacf/todlist/filter/FilterTaskAuth.java
+++ src/main/java/com/henriqueacf/todlist/filter/FilterTaskAuth.java
@@ -50,11 +50,12 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
                 if(passwordVerify.verified){
+                    // prossegue
+                    request.setAttribute("idUser", user.getId());
                     filterChain.doFilter(request, response);
                 } else {
                     response.sendError(401);
                 }
-                // prossegue
             }
         } else {
             filterChain.doFilter(request, response);
